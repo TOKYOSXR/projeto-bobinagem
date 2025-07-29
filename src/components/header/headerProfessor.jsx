f00ffc227c5b5a3772271216334aa50232321f7d
Professor-Jose <jose.igino@edu.sc.senai.br>
2025-07-29 16:17:17 -0300

    update

--- src/components/header/headerProfessor.jsx
+++ src/components/header/headerProfessor.jsx
@@ -1,6 +1,8 @@
 "use client"
 
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function HeaderProfessor() {
     const router = useRouter();
