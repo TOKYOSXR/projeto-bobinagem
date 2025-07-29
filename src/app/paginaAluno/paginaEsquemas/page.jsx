00f9b0b0c4c0a3dc088cb35c953b207c1679ef3b
TokyoSxR <juscelinor406@gmail.com>
2025-07-29 16:15:19 -0300

    começando pagina de esquemas, e componente do sidebar esquerdo

---
+++ src/app/paginaAluno/paginaEsquemas/page.jsx
@@ -0,0 +1,13 @@
+import "@/app/globals.css";
+import Header from "@/components/header/header";
+import SidebarEsquerdo from "@/components/sidebarEsquerdo";
+
+export default function PaginaEsquemas(){
+    return(
+        <>
+            <Header />
+            <SidebarEsquerdo />
+        </>
+    )
+
+}
