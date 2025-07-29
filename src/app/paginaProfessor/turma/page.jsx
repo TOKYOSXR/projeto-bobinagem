9b59b8b7d6e7e27c33df9aa193b81fc5b741d6c9
Professor-Jose <jose.igino@edu.sc.senai.br>
2025-07-29 12:56:50 -0300

    updates tela de turma

--- src/app/paginaProfessor/turma/page.jsx
+++ src/app/paginaProfessor/turma/page.jsx
@@ -1,5 +1,6 @@
 import CardTurma from "@/components/card/turma";
 import HeaderProfessor from "@/components/header/headerProfessor";
+import Image from "next/image";
 
 export default function PaginaProfessor() {
 
@@ -7,12 +8,24 @@ export default function PaginaProfessor() {
     <>
       <HeaderProfessor />
       <div className="py-10 px-20">
-        <div>
-          <p className="text-[#02335E] text-3xl">Seja bem-vindo Professor X!</p>
+        <div className="flex justify-between">
+          <p className="text-[#02335E] text-3xl font-bold">MI-75 PSIN 2023/2 </p>
+          <Image className="max-h-6"
+            src="/engrenagem.png"
+            alt="Configuração"
+            width={24}
+            height={24}
+          />
         </div>
-        <section>
-          <div>
-            <CardTurma totalCard={8} />
+        <section className="flex mt-10">
+          <div className="flex justify-between">
+            <p className="text-2xl font-medium">Alunos</p>
+
+          </div>
+
+          <div className="flex justify-between">
+            <p className="text-2xl font-medium">Alunos</p>
+
           </div>
         </section>
       </div>
